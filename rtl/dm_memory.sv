1b2b22c001fcd9a505560c9790c050a4a818c23a
Кулешов Владислав <kul.leshiy@yandex.ru>
2022-02-17 13:23:02 +0300

    Feat(dm_memoery.sv) Добавил простенький модуль памяти
    Для проверки работы кэша необходимо было выставлять ready
    от памяти, так что пускай будет такой небольшой модуль, а
    там дальше видно будет, оставлять его или нет

---
+++ rtl/dm_memory.sv
@@ -0,0 +1,25 @@
+
+import cache_def::*;
+
+module dm_memory(
+    input   logic           clk_i,
+    input   mem_req_type    mem_req_i, // cache to memory
+    output  mem_data_type   mem_data_o // memory to cache
+);
+
+logic [127:0] memory[int];
+logic [31:0]  address;
+
+assign address = mem_req.addr >> 4;
+
+always_ff @( posedge clk_i ) begin
+    mem_data_o.ready = 1'b0;
+    if (mem_req_i.valid) begin
+        mem_data_o.ready = 1'b1;
+        if(mem_req_i.rw)
+            memory[address] = mem_req_i.data;
+        else
+            mem_data_o.data = memory[address];
+    end
+end
+endmodule
